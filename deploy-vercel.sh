535d856a262ed2e0d8c6620cb3caed3e99cd1bc1
fuad-hasan-safat <fuadsafat16@gmail.com>
2024-11-10 13:52:26 +0600

    update cl

--- deploy-vercel.sh
+++ deploy-vercel.sh
@@ -7,4 +7,4 @@ curl -fsSL https://deno.land/x/install/install.sh | sh
 /vercel/.deno/bin/deno install --unstable --allow-read --allow-write --allow-net --allow-import  https://deno.land/x/pagic/mod.ts
 
 # Pagic build
-/vercel/.deno/bin/deno run --unstable --allow-read --allow-write --allow-net --allow-env --allow-run --allow-import -- --global https://deno.land/x/pagic/mod.ts build
+/vercel/.deno/bin/deno run --unstable --allow-read --allow-write --allow-net --allow-env --allow-run --allow-import https://deno.land/x/pagic/mod.ts build
