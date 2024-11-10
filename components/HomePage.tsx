e41f60e0724b2e6194d68dd0e44cce909b0691cc
fuad-hasan-safat <fuadsafat16@gmail.com>
2024-11-10 12:35:06 +0600

    k

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
-import IconContainer from './IconContainer';
+import IconContainer from "./IconContainer.tsx";
 import {
     RiFacebookBoxFill,
     RiInstagramFill,
